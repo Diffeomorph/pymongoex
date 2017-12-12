786bfa59f1363bf509a5e3b5df701004c5ca9055
gf1721 <gf1721@bristol.ac.uk>
2017-12-12 12:06:07 +0000

    first bit

---
+++ py_mongo.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask.ext.pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'connect_to_mongo'
+app.config['MONGO_URI'] = 'mongodb://gf1721:Garden12@ds135916.mlab.com:35916/connect_to_mongo'
+
+mongo = PyMongo(app)
+
+@app.route('/add')
+def add():
+    
